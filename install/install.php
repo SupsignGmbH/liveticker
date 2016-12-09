ade724a52ca509dcfe01fa7ae4befc53dd1770f9
supsign <info@supsign.ch>
2016-12-09 18:41:45 +0100

    Create install.php

---
+++ install/install.php
@@ -0,0 +1,12 @@
+<!-- Frage nach Datenbank, Datenbankbenutzer und Passwort -->
+<html>
+<head>
+</head>
+<body>
+<form action="install_sql.php" method="POST">
+<input name="db"><br />
+<input name="db_benutzer"><br />
+<input name="db_passwort"><br />
+<button type="submit">
+</form>
+</body>
